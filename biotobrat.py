9d8b79ec0352a6787298b1fedff4cedcae8b2a18
charlie <nehadil@2019@gmail.com>
2022-08-01 12:06:22 -0400

    Moving to my laptop. I am working on writing out the text to a text variable as well for checking later

--- biotobrat.py
+++ biotobrat.py
@@ -11,4 +11,17 @@ for file in os.listdir(input_dir):
     input_file = open(input_dir+"/"+file, "r")
     input_file_text = input_file.read()
     docs = input_file_text.split("-DOCSTART- X X O")
-    print(docs)
+    print(docs[1])
+    for d in docs:
+        text = ""
+        entity_text = ""
+        span = 0
+        lines = d.split("\n\n")
+        print(lines)
+        for l in lines:
+            if l != "" and l!= "** JJ O\nIGNORE NNP O\nLINE NNP O\n** . O":
+                tokens = l.split("\n")
+                for t in range(len(tokens)):
+                    subtokens = tokens[t].split(" ")
+                    text+=subtokens[0]
+                    if t!=len(tokens)-1:
