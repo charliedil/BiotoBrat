476ab17f46294b684668aa6c1f4640a5e3ce0e53
charliedil <charliedil2019@gmail.com>
2022-08-01 15:58:00 -0400

    It appears to be working - huzzah! Need to document code next

--- biotobrat.py
+++ biotobrat.py
@@ -10,12 +10,18 @@ entity = sys.argv[3]
 for file in os.listdir(input_dir):
     input_file = open(input_dir+"/"+file, "r")
     input_file_text = input_file.read()
+    input_file.close()
     docs = input_file_text.split("-DOCSTART- X X O")
     print(docs[1])
+    filename = 0
     for d in docs:
+
+        counter = 0
+        entities = {}
         text = ""
         entity_text = ""
         span = 0
+        start_span = span
         lines = d.split("\n\n")
         print(lines)
         for l in lines:
@@ -23,5 +29,37 @@ for file in os.listdir(input_dir):
                 tokens = l.split("\n")
                 for t in range(len(tokens)):
                     subtokens = tokens[t].split(" ")
+                    if subtokens[2].split("-")[0] == "B" and entity_text !="":
+                        entities["T" + str(counter)] = [entity_text, start_span, start_span + len(entity_text)]
+                        start_span = span
+                        entity_text = ""
+                        entity_text += subtokens[0]
+                        counter+=1
+                    if subtokens[2].split("-")[0] == "B" and entity_text =="":
+                        entity_text += subtokens[0]
+                        start_span = span
+                    if subtokens[2].split("-")[0] == "I":
+                        entity_text +=" "+subtokens[0]
+                    if subtokens[2].split("-")[0] == "O" and entity_text!="":
+                        entities["T"+str(counter)] = [entity_text, start_span, start_span+len(entity_text)]
+                        start_span = span
+                        entity_text = ""
+                        counter+=1
+
                     text+=subtokens[0]
+                    span+=len(subtokens[0])
                     if t!=len(tokens)-1:
+                        text += " "
+                        span+=1
+                    else:
+                        text+="\n"
+                        span+=1
+        ann_file = open(output_dir+"/"+str(filename)+".ann", "w")
+        for e in entities:
+            ann_file.write(e+"\t"+entity+" "+str(entities[e][1])+" "+str(entities[e][2])+"\t"+entities[e][0]+"\n")
+        text_file = open(output_dir+"/"+str(filename)+".txt", "w")
+        text_file.write(text)
+        text_file.close()
+        ann_file.close()
+        filename+=1
+
